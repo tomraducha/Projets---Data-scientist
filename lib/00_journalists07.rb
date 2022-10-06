1a6f94286c52ce819d1be8f8af0f02a06d33fa92
Thomas Raducha <thomasraducha@gmail.com>
2022-10-06 13:33:53 +0200

    second commit

---
+++ lib/00_journalists07.rb
@@ -0,0 +1,5 @@
+#Quelle est la position dans l'array de la personne @epenser ?
+
+hash = Hash[array.map.with_index.to_a] # => {"a"=>0, "b"=>1, "c"=>2} hash['b'] # => 1
+hash[ class="hljs-string">'b'] # => 1
+
